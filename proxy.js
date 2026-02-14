1acffdbd4917db6f9bf5e466feef4b4c678bda38
Muzammil Husnain <muzammilhusnain7@gmail.com>
2026-02-14 12:33:05 +0500

    Fix Next 16 useSearchParams build error

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
+export async function proxy(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const url = req.nextUrl.clone();
 
